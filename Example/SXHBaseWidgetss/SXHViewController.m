d183497bd3d92783c70b5884e45db47d5b40a588
shiyong <shiyong@trip.com>
2022-02-21 10:36:35 +0800

    这是一条新的提交纪录

--- Example/SXHBaseWidgetss/SXHViewController.m
+++ Example/SXHBaseWidgetss/SXHViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"这是一条新纪录");
 }
 
 - (void)didReceiveMemoryWarning
